c1cbedc322d2437b9e924f740bb4d1b8080fa7fe
minami.yoshihiko <nannany@hey.jp>
2023-02-13 23:27:34 +0900

    vpc作成

--- src/main/java/com/myorg/SimpleAppByCdkStack.java
+++ src/main/java/com/myorg/SimpleAppByCdkStack.java
@@ -1,5 +1,9 @@
 package com.myorg;
 
+import com.sun.tools.javac.util.List;
+
+import software.amazon.awscdk.Tags;
+import software.amazon.awscdk.services.ec2.Vpc;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
@@ -14,7 +18,11 @@ public class SimpleAppByCdkStack extends Stack {
     public SimpleAppByCdkStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        final Vpc vpc = Vpc.Builder.create(this, "public-vpc")
+                .vpcName("simple-app-public-vpc")
+                .build();
+
+        Tags.of(vpc).add("created_by", "cdk");
 
         // example resource
         // final Queue queue = Queue.Builder.create(this, "SimpleAppByCdkQueue")
