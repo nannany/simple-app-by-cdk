ef4c3c5d250f431ad6506a8cbfedc85d1700c2b5
minami.yoshihiko <nannany@hey.jp>
2023-02-15 00:01:53 +0900

    availability zoneがなくて怒られる

--- src/main/java/com/myorg/SimpleAppByCdkStack.java
+++ src/main/java/com/myorg/SimpleAppByCdkStack.java
@@ -2,8 +2,14 @@ package com.myorg;
 
 import com.sun.tools.javac.util.List;
 
+import jdk.internal.loader.AbstractClassLoaderValue;
 import software.amazon.awscdk.Tags;
+import software.amazon.awscdk.services.ec2.CfnInternetGateway;
+import software.amazon.awscdk.services.ec2.CfnInternetGatewayProps;
+import software.amazon.awscdk.services.ec2.Subnet;
+import software.amazon.awscdk.services.ec2.SubnetProps;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ec2.VpcProps;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
@@ -18,11 +24,19 @@ public class SimpleAppByCdkStack extends Stack {
     public SimpleAppByCdkStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        final Vpc vpc = Vpc.Builder.create(this, "public-vpc")
-                .vpcName("simple-app-public-vpc")
-                .build();
+        final Vpc vpc = new Vpc(this, "vpc"
+                , VpcProps.builder().vpcName("simple-app-public-vpc").build());
 
-        Tags.of(vpc).add("created_by", "cdk");
+        final Subnet publicSubnet = new Subnet(this, "public-subnet"
+                , SubnetProps.builder().vpcId(vpc.getVpcId()).build());
+
+        final Subnet privateSubnet = new Subnet(this, "private-subnet"
+                , SubnetProps.builder().vpcId(vpc.getVpcId()).build());
+
+        final CfnInternetGateway internetGateway = new CfnInternetGateway(this, "internet-gateway"
+                , CfnInternetGatewayProps.builder().build());
+
+        publicSubnet.addDefaultInternetRoute(internetGateway.getLogicalId(), publicSubnet);
 
         // example resource
         // final Queue queue = Queue.Builder.create(this, "SimpleAppByCdkQueue")
